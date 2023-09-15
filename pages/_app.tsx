ab0d3b82973737cc57e09fb340eb55c71e82dc59
ngunjiri <owen2ngunjiri@gmail.com>
2023-09-15 16:36:34 +0300

    updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import localFont from "next/font/local";
 import type { AppProps } from "next/app";
+
 const proximaFont = localFont({ src: "Proxima.otf", variable: '--Proxima-Nova-Rg' });
 export default function App({ Component, pageProps }: AppProps) {
   return (
